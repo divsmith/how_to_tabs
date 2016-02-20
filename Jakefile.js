b4129db2a0a6cba0b1e58f9d1d393ca7237c522b
Parker Smith <parker@parkersmith.us>
2016-02-20 15:23:33 -0700

    Lint all JS files in src directory"

--- Jakefile.js
+++ Jakefile.js
@@ -39,7 +39,7 @@
 		process.stdout.write("Linting Javascript: ");
 
 		jshint.checkFiles({
-			files: "Jakefile.js",
+			files: [ "Jakefile.js", "src/**/*.js" ],
 			options: {
 				bitwise: true,
 				eqeqeq: true,
